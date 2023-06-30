d702c0473efaf87b9bdf8cd112a4185f9a5abd08
SamWylde <tdarbylhu@gmail.com>
2023-06-30 14:02:34 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,17 +3,17 @@
 store_version = 8  # Needed for dynamic plugin loading
 
 __license__ = "GPLv3"
-__copyright__ = "poochinski9"
+__copyright__ = "Thomas Darby"
 __docformat__ = "restructuredtext en"
 
 from calibre.customize import StoreBase
 
 
 class LibgenStore(StoreBase):
-    name = "Library Genesis"
+    name = "Library Genesis Fiction"
     version = (1, 2, 0)
-    description = "Searches for books on Library Genesis"
-    author = "poochinski9"
+    description = "Searches for fiction books on Library Genesis"
+    author = "Thomas Darby"
     drm_free_only = True
     actual_plugin = "calibre_plugins.store_libgen.libgen_plugin:LibgenStorePlugin"
     formats = ["EPUB", "PDF"]
